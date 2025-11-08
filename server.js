1b6626490a10561db33b417bebf228b27dd4ec7e
biixin <113357477+biixin@users.noreply.github.com>
2025-11-08 15:14:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 // Troca esses números pelos seus reais
 const numbers = [
-  "5521965701466"
+  "5521965701466",
+  "5511977877209"
 ];
 
 const REDIS_URL = process.env.REDIS_URL;
